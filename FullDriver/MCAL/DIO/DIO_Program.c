b78509225eed0c82737905820c7c06c30110144e
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-09-03 20:32:46 +0300

    Writing Doxygen Comments On Functions Declarations and adding the functions with empty implementaions in program.c

--- FullDriver/MCAL/DIO/DIO_Program.c
+++ FullDriver/MCAL/DIO/DIO_Program.c
@@ -6,3 +6,24 @@
  * @version  .1
  * @date     3/9/2025
  */
+#include "DIO_Interface.h"
+void mDIO_ChangeDirectionForPin(uint8_t PortGroup, uint8_t PinNumber, uint8_t Direction)
+{
+}
+void mDIO_ChangeDirectionForGroup(uint8_t PortGroup, uint8_t Direction)
+{
+}
+
+void mDIO_WriteValueForPin(uint8_t PortGroup, uint8_t PinNumber, uint8_t OutputValue)
+{
+}
+void mDIO_WriteValueForGroup(uint8_t PortGroup, uint8_t OutputValue)
+{
+}
+
+uint8_t mDIO_ReadValueFromPin(uint8_t PortGroup, uint8_t PinNumber)
+{
+}
+uint8_t mDIO_ReadValueFromGroup(uint8_t PortGroup)
+{
+}
