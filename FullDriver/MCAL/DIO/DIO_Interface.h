ffb808f71a80a0ec82e5f65ad0ee93d1bd701274
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-09-03 20:17:06 +0300

    Adding the Config file in Folder Common and adding Doxygen Comments on DIO Files

--- FullDriver/MCAL/DIO/DIO_Interface.h
+++ FullDriver/MCAL/DIO/DIO_Interface.h
@@ -1,3 +1,11 @@
+/**
+ * @file   DIO_Private.h
+ * @author Developer: Mohamed Diaa -> MohammedDiaato@gmail.com
+ * @author Reviewer:  Hesham Ahmed ->
+ * @brief This file contains The dio Private values
+ * @version  .1
+ * @date     3/9/2025
+ */
 #ifndef _DIO_INTERFACE_H_
 #define _DIO_INTERFACE_H_
 #endif
---
+++ FullDriver/MCAL/DIO/DIO_Program.c
@@ -0,0 +1,10 @@
+/**
+ * @file   DIO_Private.h
+ * @author Developer: Mohamed Diaa -> MohammedDiaato@gmail.com
+ * @author Reviewer:  Hesham Ahmed ->
+ * @brief This file contains The dio Private values
+ * @version  .1
+ * @date     3/9/2025
+ */
+
+ 
