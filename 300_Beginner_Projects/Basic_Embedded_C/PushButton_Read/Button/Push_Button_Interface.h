c6b3eecd10354774b664197d0844d66957155155
Mohamed Diaa <mohammeddiaato@gmail.com>
2025-10-14 00:39:36 +0300

    Initializing the program files and basic Doxygen Comments

---
+++ 300_Beginner_Projects/Basic_Embedded_C/PushButton_Read/Button/Push_Button_Interface.h
@@ -0,0 +1,7 @@
+/**
+ * @file    Push_Button_Interface.h
+ * @brief   This File Includes APIs used for the Functionality of reading push button state with various kinds of connections.
+ * @details This file include 
+ * @author
+ * @version 1
+ */
